1695bf024bf0eac5a983dfef381c5e2c4c80e996
Diego Grassino <diegograssino@gmail.com>
2022-07-11 21:27:48 -0300

    Fin clase 4

--- clases/clase2.js
+++ clases/clase2.js
@@ -21,7 +21,6 @@
 // let string123= `${string1} ${string2} ${string3}`
 // console.log(string123)
 
-
 // Lleve una cadena de caracteres en minuscula a mayusculas.
 
 // let minusculas = "metallica";
@@ -63,14 +62,11 @@
 
 // Si LlenoTodosLosCampos = true, entonces que ingrese a la aplicacion, sino, si LlenoAlgunCampo = true mostrar error con lo que falta, y sino, Mostrar error que falta todo.
 
-
-
 // Extraiga el 5to caractér del string "SILENCIOSOS"
 // let nombre = prompt ("Ingrese un nombre: ")
 
 // let caracterAExtraer = prompt("Que caracter de SILENCIOSOS desea extraer?")
 // alert("La inicial del nombre es " + nombre.charAt(0))
-
 
 // Del mismo string, extraiga las últimas dos letras.
 
@@ -95,12 +91,10 @@
 
 // let varMaxNumeros = Math.max(1, 5, 1000, 2, 10)
 
-
 // alert("El mayor es: " + varMax)
 // alert("El menor es: " + varMin)
 
 // console.log(varMaxNumeros)
-
 
 // Con las mismas variables, devuelva el mínimo
 
@@ -121,108 +115,248 @@
 
 // console.log("El número aleatorio del 1 al " + max + " es: " + numeroAleatorioCustom)
 
-
 // A ese número redondearlo a dos decimales
 
-// 
-
+//
 
 // 4)
 // Dado un número, indicar si ese número es mayor a 10.
 
-// let numero = prompt("Ingrese su número");
+// let numero = 11;
+// let numero = prompt('Ingrese un número: ');
 
 // if (numero > 10) {
-
+// console.log(numero + ' es mayor a 10.');
+//   alert(numero + ' es mayor a 10.');
+// }
 
 // Dado un número, indicar si ese número es menor o igual a 10.
 
-// let numero = prompt("Ingrese un número:");
+// let numero = 20;
 
+// let numero = prompt('Ingrese un número: ');
+
+// if (numero <= 10) {
+//   alert(numero + ' es menor o igual a 10.');
+// }
+
+// if (numero > 10) {
+//   alert(numero + ' es mayor a 10.');
+// }
 
 // Dado un número, indicar si ese número es par.
 
-// let numero = prompt("Ingrese un número: ");
+// let numero = 6;
 
+// if (numero % 2 === 0) {
+//   console.log('El número es par');
+// }
+
+// if (numero % 2 !== 0) {
+//   console.log('El número es impar');
+// }
 
 // Dado un número, indicar si ese número es impar.
 
-// let numero = prompt("Ingrese un número: ");
-
-
 // Ante un valor ingresado, indicar si tiene contenido.
 
-// let valor = prompt("Ingrese un valor:");
+// let numero = '';
 
+// console.log(numero.length);
+
+// if (numero.length) {
+//   console.log('numero tiene contenido.');
+// }
+
+// if (!numero.length) {
+//   console.log('numero no tiene contenido.');
+// }
 
 // 5)
 
-// Dado un número, indicar si ese número es mayor o menor o igual 
+// Dado un número, indicar si ese número es mayor o menor o igual
 // a 10.
 
+// let numero = prompt('Ingrese un número: ');
+
+// if (numero > 10) {
+//   alert('Es mayor a 10');
+// } else if (numero < 10) {
+//   alert('Es menor a 10');
+// } else {
+//   alert('Es igual 10');
+// }
+
+// if (numero > 10) {
+//   alert('Es mayor a 10');
+// }
+// if (numero < 10) {
+//   alert('Es menor a 10');
+// }
+// if ((numero = 10)) {
+//   alert('Es igual a 10');
+// }
 
 // Dado un número, indicar si ese número es par o impar.
 
-// let numero = prompt("Ingrese un número");
-
-
 // Dados dos números, indicar cual es el mayor.
 
-
 // Ante un valor ingresado, indicar si tiene o no contenido.
-
-// TAREA!
 
 // 6)
 
 // Dados dos números, indicar cual es mayor o si son iguales.
 
+// let numero1 = prompt(
+//   'Ingrese el primer número: '
+// );
+// let numero2 = prompt(
+//   'Ingrese el segundo número: '
+// );
 
+// if (numero1 === numero2) {
+//   alert('Son iguales');
+// } else if (numero1 > numero2) {
+//   alert('El primer número es mayor');
+// } else {
+//   alert('El segundo número es mayor');
+// }
 
 // Pedir las tres notas de un alumno y calcular su promedio, si es mayor
 // a 6, mostrar que aprobó, si es menor a 6, mostrar que desaprobó
 // y si es igual a 6, mostrar que está en condiciones de recuperación.
 
+// Prompt siempre me devuelve un string
+// let nota1string = prompt('Ingrese primer nota: ');
+
+// let nota1 = parseFloat(nota1string);
+
+// let nota2 = parseFloat(
+//   prompt('Ingrese segunda nota: ')
+// );
+// let nota3 = parseFloat(
+//   prompt('Ingrese tercer nota: ')
+// );
+
+// let promedio = (nota1 + nota2 + nota3) / 3;
+
+// console.log(promedio);
+
+//TAREA
+// 7)
 // Crear un algoritmo que pida al usuario el diámetro de una rueda
 // y su grosor (en metros) y realice las siguientes operaciones:
-
 
 // a) Si el diámetro es superior a 1.4 debe mostrarse el mensaje
 //“La rueda es para un vehículo grande”. Si es menor o igual a 1.4
 // pero mayor que 0.8 debe mostrarse el mensaje “La rueda es para
 // un vehículo mediano”. Si no se cumplen ninguna de las condiciones
-// anteriores debe mostrarse por pantalla el mensaje “La rueda es 
+// anteriores debe mostrarse por pantalla el mensaje “La rueda es
 //para un vehículo pequeño”.
-
-// Solicitar un numero de mes y mostrar el nombre del mes
-// correspondiente.
-
-// Solicitar un numero de día y mostrar el nombre del día
-
-
-// 7)
 
 // b) Si el diámetro es superior a 1.4 con un grosor inferior a 0.4,
 // ó si el diámetro es menor o igual a 1.4 pero mayor que 0.8, con un
 // grosor inferior a 0.25, deberá mostrarse el mensaje “El grosor para
 // esta rueda es inferior al recomendado”
 
+// Solicitar un numero de mes y mostrar el nombre del mes
+// correspondiente.
+
+// let mes = 20;
+
+// if (mes === 1) {
+//   console.log('El mes es Enero');
+// } else if (mes === 2) {
+//   console.log('El mes es Febrero');
+// } else if (mes === 3) {
+//   console.log('El mes es Marzo');
+// } else if (mes === 4) {
+//   console.log('El mes es Abril');
+// } else if (mes === 5) {
+//   console.log('El mes es Mayo');
+// } else if (mes === 6) {
+//   console.log('El mes es Junio');
+// } else if (mes === 7) {
+//   console.log('El mes es Julio');
+// } else if (mes === 8) {
+//   console.log('El mes es Agosto');
+// } else if (mes === 9) {
+//   console.log('El mes es Septiembre');
+// } else if (mes === 10) {
+//   console.log('El mes es Octubre');
+// } else if (mes === 11) {
+//   console.log('El mes es Noviembre');
+// } else if (mes === 12) {
+//   console.log('El mes es Diciembre');
+// } else {
+//   console.log('No corresponde a ningún mes');
+// }
+
+// TAREA
+
+// Solicitar un numero de día y mostrar el nombre del día
 
 // 8)
-
+// TAREA
 // Hacer ahora con Switch:
 // Pedir las tres notas de un alumno y calcular su promedio, si es mayor
 // a 6, mostrar que aprobó, si es menor a 6, mostrar que desaprobó
 // y si es igual a 6, mostrar que está en condiciones de recuperación.
 
-
 // Hacer ahora con Switch:
 // Solicitar un numero de mes y mostrar el nombre del mes
 // correspondiente.
 
+// let mes = 6;
+
+// switch (mes) {
+//   case 1:
+//     console.log('Enero');
+//     break;
+//   case 2:
+//     console.log('Febrero');
+//     break;
+//   case 3:
+//     console.log('Marzo');
+//     break;
+//   case 4:
+//     console.log('Abril');
+//     break;
+//   case 5:
+//     console.log('Mayo');
+//     break;
+//   case 6:
+//     console.log('Junio');
+//     break;
+//   case 7:
+//     console.log('Julio');
+//     break;
+//   case 8:
+//     console.log('Agosto');
+//     break;
+//   case 9:
+//     console.log('Septiembre');
+//     break;
+//   case 10:
+//     console.log('Octubre');
+//     break;
+//   case 11:
+//     console.log('Noviembre');
+//     break;
+//   case 12:
+//     console.log('Diciembre');
+//     break;
+
+//   default:
+//     console.log('No corresponde a un mes.');
+//     break;
+// }
+
+// TAREA
 // Hacer ahora con Switch:
 // Solicitar un numero de día y mostrar el nombre del día
 
+// TAREA
 // Escribe un programa que responda a un usuario que quiere comprar
 // un helado en una conocida marca de comida rápida cuanto le costará
 // en función del topping que elija. Si no hay concidencias debe
@@ -232,5 +366,7 @@
 // El topping de KitKat cuesta $175.
 // El topping de brownie cuesta $175.
 
+// TAREA
 // Solicitar al usuario un modelo de auto que desea comprar e
+// TAREA
 // informarle cuánto cuesta. Si no hay concidencias debe informarlo.
