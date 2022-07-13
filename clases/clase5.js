72b0b4c7b1fda235357333825b3bf3b557780438
Diego Grassino <diegograssino@gmail.com>
2022-07-13 20:05:54 -0300

    Agregados antes de empezar la clase

---
+++ clases/clase5.js
@@ -0,0 +1,21 @@
+// CONDICIONALES: Condiciones compuestas (&&, ||, !)
+
+// Escriba un algoritmo que pueda evaluar si un número esta dentro del rango de 5 a 10.
+
+// Simule un estado de logueo con usuario y contraseña y, solo si no esta logueado, muestre un mensaje de error.
+
+// Simule un ingreso de un numero de telefono en un formulario y valide si el dato es correcto por la cantidad de digitos.
+
+// 2)
+
+// (AHORA CON FOR) Escribe un algoritmo que muestre los numeros del 1 al 10.
+
+// (AHORA CON FOR) Escribe un algoritmo que muestre los numeros del 1 al 10 de mayor a menor.
+
+// (AHORA CON FOR) Escribe un algoritmo que muestre si cada uno de los numeros del 1 al 10 son múltiplos de 2.
+
+// Escribe un algoritmo que muestre solo los numeros pares del 1 al 10
+
+// Genere CON LAS HERRAMIENTAS QUE TENEMOS HASTA AHORA un juego de 10 preguntas y respuestas donde cada respuesta correcta sume un punto. Al final del juego debe mostrar cuantos puntos sumo el jugador.
+
+// Genere CON LAS HERRAMIENTAS QUE TENEMOS HASTA AHORA un algoritmo que pida dos numeros y la operacion a realizar (+, -, *, /) y muestre el resultado. Validar que los datos ingresados sean correctos y volver a pedirlos si es necesario.
