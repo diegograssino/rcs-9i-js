8297e244534205ca1ec5bb7322f9b0e4e3a54446
Diego Grassino <diegograssino@gmail.com>
2022-08-03 21:06:26 -0300

    Fin de la clase

---
+++ temas/tema7.js
@@ -0,0 +1,63 @@
+//  2)
+
+const divHtml =
+  document.getElementById('rolling');
+// console.log(divHtml);
+
+const divHtml2 =
+  document.querySelector('.rolling2');
+// console.log(divHtml2);
+
+// 3)
+// divHtml.style.backgroundColor = '#000';
+// divHtml.style.width = '100%';
+// divHtml.style.height = '24px';
+
+// divHtml2.className += ' roberto';
+
+// 4)
+
+// console.log(divHtml.innerHTML);
+
+// divHtml.innerHTML = 'Hola Rolling';
+// console.log(divHtml.innerHTML);
+
+// divHtml.innerText = 'Hola Rolling con innerText';
+
+// divHtml.innerHTML =
+//   '<h2>Hola Rolling con innerHtml</h2>';
+
+// const textoDePrueba = 'probando2';
+
+// divHtml.innerHTML = `Hola Rolling con template literals ${textoDePrueba}`;
+
+// const inputHtml = document.getElementById('data');
+
+// console.log(inputHtml.value);
+
+// const nuevoH2 = document.createElement('h2');
+// console.log(nuevoH2);
+// nuevoH2.innerText = 'Hola Rolling';
+
+// divHtml2.appendChild(nuevoH2);
+
+// data = [
+//   { nombre: 'nombre1', precio: 1 },
+//   { nombre: 'nombre2', precio: 2 },
+//   { nombre: 'nombre3', precio: 3 },
+// ];
+
+// data.forEach(d => {
+//   const titulo = document.createElement('h4');
+//   titulo.innerText = d.nombre;
+//   const precio = document.createElement('span');
+//   precio.innerText = d.precio;
+
+//   const card = document.createElement('div');
+//   card.appendChild(titulo);
+//   card.appendChild(precio);
+
+//   card.style.border = '1px solid #FFF';
+
+//   divHtml.appendChild(card);
+// });
